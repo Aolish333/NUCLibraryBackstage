30cb2defb2daa3f2635f9d10855671f405b9ed84
Aolish333 <aolish333@gmail.com>
2018-01-11 13:04:53 +0800

    Initial commit

---
+++ src/main/java/com/nuclibrarybackstage/NucLibraryBackstageApplication.java
@@ -0,0 +1,12 @@
+package com.nuclibrarybackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucLibraryBackstageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NucLibraryBackstageApplication.class, args);
+	}
+}
